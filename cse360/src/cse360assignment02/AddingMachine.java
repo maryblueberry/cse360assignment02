be7dc882525e111bd7cddedccf258da2b4f50608
Apple <mrando1@asu.edu>
2020-10-05 20:14:55 -0700

    Initial commit

---
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,75 @@
+package cse360assignment02;
+	
+/**
+	 * 
+	 * The Adding Machine program will do a quick calculation- 
+	 * add and/or subtract to return the total integer value 
+	 * while keeping a history of the transactions made and clearing the memory.
+	 * 
+	 * 
+	 * @author Maribel Rando 
+	 * @version 1.0
+	 * @since 2020-05-10 
+	 * 
+	 */
+
+	public class AddingMachine {
+
+			  private int total;
+			  
+			  /** 
+			   *  This method is used to implement the variable total 
+			   *  
+			   */
+			  public AddingMachine () 
+			  {
+			    total = 0;  // not needed - included for clarity
+			  }
+			  
+			  /**
+			   * This method is used to get the total amount  
+			   * @return 0 
+			   */
+			  public int getTotal () 
+			  {
+			    return 0;
+			  }
+			  
+			  /** 
+			  * This method is used to conduct addition 
+			  * @param value This is the first parameter to the add method
+			  * @return Nothing 
+			  */
+			  public void add (int value) 
+			  {
+				  
+			  }
+
+			  /**
+			  * This method is used to conduct subtraction with the value parameter 
+			  * @param value This is the first parameter to the subtract method
+			  * @return Nothing
+			  */
+			  public void subtract (int value) 
+			  {
+				  
+			  }
+
+			  /**
+			  * This method is used to return the String
+			  * @return 
+			  */
+			  public String toString () 
+			  {
+			    return "";
+			  }
+			  
+			  /** 
+			  * This method is used to clear the memory
+			  * @return Nothing
+			  */
+			  public void clear() 
+			  {
+				  
+			  }
+}
